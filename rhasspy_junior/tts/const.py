20ca322e1f3625ad248bed0c8d66cfdc808f773a
Michael Hansen <mike@rhasspy.org>
2022-06-02 17:18:44 -0400

    Add TTS base class

---
+++ rhasspy_junior/tts/const.py
@@ -0,0 +1,57 @@
+# Copyright 2022 Michael Hansen
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import typing
+from abc import abstractmethod
+from dataclasses import dataclass
+
+from rhasspy_junior.const import ConfigurableComponent
+
+
+@dataclass
+class TextToSpeechRequest:
+    """Request for audio synthesis from text"""
+
+    text: str
+
+
+@dataclass
+class TextToSpeechResult:
+    """Result of synthesis"""
+
+    wav_bytes: bytes
+    """WAV audio"""
+
+
+class TextToSpeech(ConfigurableComponent):
+    """Base class for text to speech engines"""
+
+    def __init__(
+        self,
+        root_config: typing.Dict[str, typing.Any],
+        config_extra_path: typing.Optional[str] = None,
+    ):
+        super().__init__(root_config, config_extra_path=config_extra_path)
+
+    @abstractmethod
+    def synthesize(self, request: TextToSpeechRequest) -> TextToSpeechResult:
+        """Synthesize audio from text"""
+
+    def start(self):
+        """Initialize engine"""
+
+    def stop(self):
+        """Uninitialize engine"""
